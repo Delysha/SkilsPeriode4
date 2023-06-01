e82ab78e50c77a0afd42de4908baa97a5a134f61
delysha <34533@ma-web.nl>
2023-06-01 14:50:08 +0200

    arrays

---
+++ arrays.php
@@ -0,0 +1,9 @@
+<?php
+$arr = array(1, 2 , 3 , 4 , 5);
+//$arr[] = 3;
+//print_r($arr);
+var_dump($arr);
+echo $arr[0];
+
+ $x = 1;
+echo $arr [$x];  //<-- hier bepaal je de positie waar die mee begint 
